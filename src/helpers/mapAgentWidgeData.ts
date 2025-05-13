9994ac649334fe81d479f959b21a575820a0c1bb
Gijsjan Brouwer <agijsbro@gmail.com>
2025-05-13 11:45:38 +0200

    Remove duplicate code and use functional methods on mapping agent data

--- src/helpers/mapAgentWidgeData.ts
+++ src/helpers/mapAgentWidgeData.ts
@@ -1,107 +1,70 @@
 import {
-  MainWidgetType,
-  MappedWidgetType,
-  WidgetSubType,
-} from '../types/mainWidgetData';
-import { AgentCategory } from '../types/categories';
-import { GridCategory, GridItem } from '../types/grid';
-import { AGENT_FIELD_LABELS } from '../constants/fieldLabels';
+	MainWidgetType,
+	MappedWidgetType,
+	WidgetSubType,
+} from "../types/mainWidgetData"
+import { AgentCategory } from "../types/categories"
+import { GridCategory, GridItem } from "../types/grid"
+import { AGENT_FIELD_LABELS } from "../constants/fieldLabels"
 
-const keysToExclude = ['manifestation', 'work'];
+const keysToExclude = ["manifestation", "work", AgentCategory.Agent, "title"]
 
 export function mapAgentWidgetData(data: MainWidgetType): MappedWidgetType {
-  if (!data || typeof data !== 'object') {
-    return {
-      mappedData: null,
-      error: true,
-    };
-  }
+	if (!data || typeof data !== "object") {
+		return {
+			mappedData: null,
+			error: true,
+		}
+	}
 
-  const items: GridItem[] = [];
-  Object.keys(data).forEach((key) => {
-    if (!data[key]) {
-      return;
-    }
+	const items: GridItem[] = Object.entries(data)
+		.filter(([key, value]) => value != null && !keysToExclude.includes(key))
+		.map(([key, value]) => {
+			if (
+				key == AgentCategory.Role ||
+				key == AgentCategory.Datebirth ||
+				key == AgentCategory.Placebirth ||
+				key == AgentCategory.Datedeath ||
+				key == AgentCategory.Placedeath
+			) {
+				return {
+					key: AGENT_FIELD_LABELS[key],
+					value,
+					type: GridCategory.Static,
+				}
+			}
 
-    if (keysToExclude.includes(key)) {
-      return;
-    }
+			if (key == AgentCategory.Note) {
+				return {
+					key: AGENT_FIELD_LABELS[key],
+					value: "Bio",
+					note: value,
+					type: GridCategory.Information,
+				}
+			}
 
-    if (key == AgentCategory.Role) {
-      items.push({
-        key: AGENT_FIELD_LABELS[key],
-        value: data[key],
-        type: GridCategory.Static,
-      });
-      return;
-    }
+			console.warn(
+				`Key ${key} with value ${value} is not handled in mapAgentWidgetData.
+				Please check the mapping logic.`,
+			)
+		})
+		.filter((item) => item != null) as GridItem[]
 
-    if (key == AgentCategory.Datebirth) {
-      items.push({
-        key: AGENT_FIELD_LABELS[key],
-        value: data[key],
-        type: GridCategory.Static,
-      });
-      return;
-    }
+	if (data[AgentCategory.Agent]) {
+		items.push({
+			key: "Meer werk van",
+			value: data?.title,
+			type: GridCategory.More,
+			subType: WidgetSubType.WorksForAgent,
+			id: data?.agent || "",
+		})
+	}
 
-    if (key == AgentCategory.Placebirth) {
-      items.push({
-        key: AGENT_FIELD_LABELS[key],
-        value: data[key],
-        type: GridCategory.Static,
-      });
-      return;
-    }
-
-    if (key == AgentCategory.Datedeath) {
-      items.push({
-        key: AGENT_FIELD_LABELS[key],
-        value: data[key],
-        type: GridCategory.Static,
-      });
-      return;
-    }
-
-    if (key == AgentCategory.Placedeath) {
-      items.push({
-        key: AGENT_FIELD_LABELS[key],
-        value: data[key],
-        type: GridCategory.Static,
-      });
-      return;
-    }
-
-    if (key == AgentCategory.Note) {
-      items.push({
-        key: AGENT_FIELD_LABELS[key],
-        value: 'Bio',
-        note: data[key],
-        type: GridCategory.Information,
-      });
-      return;
-    }
-  });
-
-  if (data[AgentCategory.Agent]) {
-    items.push({
-      key: 'Meer werk van',
-      value: data?.title,
-      type: GridCategory.More,
-      subType: WidgetSubType.WorksForAgent,
-      id: data?.agent || '',
-    });
-  }
-
-  const filteredResults = items.filter(
-    (item) => item !== null
-  ) as GridItem[];
-
-  return {
-    mappedData: {
-      title: data.title || '',
-      items: filteredResults,
-    },
-    error: false,
-  };
+	return {
+		mappedData: {
+			title: data.title ?? "",
+			items
+		},
+		error: false,
+	}
 }
