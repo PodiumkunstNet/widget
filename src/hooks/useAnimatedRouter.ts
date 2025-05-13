36ac68e132251bab892a1886d3a53a0d1580096e
Gijsjan Brouwer <agijsbro@gmail.com>
2025-05-13 11:48:08 +0200

    Clean up useAnimatedRouter

--- src/hooks/useAnimatedRouter.ts
+++ src/hooks/useAnimatedRouter.ts
@@ -5,18 +5,15 @@ export interface ExtendedDocument extends Document {
 }
 
 export default function useAnimatedRouter() {
-	const navigate = useNavigate()
+	const reactRouterNavigate = useNavigate()
 
-	function _navigate(url: string) {
+	function navigate(url: string) {
 		const extendedDocument = document as ExtendedDocument
 		if (!extendedDocument.startViewTransition) {
-			// return (window.location.href = url)
-			navigate(url)
+			reactRouterNavigate(url)
 		} else {
-			console.log('starting view transition')
 			extendedDocument.startViewTransition(() => {
-				// window.location.href = url
-				navigate(url)
+				reactRouterNavigate(url)
 			})
 		}
 	}
@@ -24,47 +21,13 @@ export default function useAnimatedRouter() {
 	function back() {
 		const extendedDocument = document as ExtendedDocument
 		if (!extendedDocument.startViewTransition) {
-			// return window.history.back()
-			navigate(-1)
+			reactRouterNavigate(-1)
 		} else {
 			extendedDocument.startViewTransition(() => {
-				// window.history.back()
-				navigate(-1)
+				reactRouterNavigate(-1)
 			})
 		}
 	}
 
-	// // Navigate to the new route
-	// const animatedRoute = (url: string) => {
-	// 	const extendedDocument = document as ExtendedDocument
-	// 	if (!extendedDocument.startViewTransition) {
-	// 		return (window.location.href = url)
-	// 	} else {
-	// 		extendedDocument.startViewTransition(() => {
-	// 			window.location.href = url
-	// 		})
-	// 	}
-	// }
-
-	// const animatedBackRoute = () => {
-	// 	const extendedDocument = document as ExtendedDocument
-	// 	if (!extendedDocument.startViewTransition) {
-	// 		return window.history.back()
-	// 	} else {
-	// 		extendedDocument.startViewTransition(() => {
-	// 			window.history.back()
-	// 		})
-	// 	}
-	// }
-
-	return { navigate: _navigate, back }
+	return { navigate, back }
 }
-
-// export function viewTransitionsStatus() {
-// 	const extendedDocument = document as ExtendedDocument
-// 	let status = "Opss, Your browser doesn't support View Transitions API"
-// 	if (extendedDocument?.startViewTransition) {
-// 		status = "Yess, Your browser support View Transitions API"
-// 	}
-// 	return status
-// }
